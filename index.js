749ad79841f2860ac41d269aea55a2ea189277cf
xScoopy <JLeary070@gmail.com>
2021-09-23 22:16:50 -0500

    added functionality for nicknames and user is typing

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('welcome user');
+    socket.on('chat message', (msg, nickname) => {
+        io.emit('chat message', msg, nickname);
+    })
+    socket.on('user typing', (nickname) => {
+        io.emit('user typing', nickname)
+    })
+})
 
 server.listen(3000, () => {
     console.log('listening on *:3000');
